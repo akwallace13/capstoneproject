057c3d3ff945a4e3f8651491c4dda857fdeaf25c
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2024-04-22 18:34:58 -0500

    modified:   hangman.css 	modified:   hangman.html 	modified:   hangman.js 	modified:   index.html 	modified:   index.js 	new file:   wordle5.css 	new file:   wordle5.html 	new file:   wordle5.js 	new file:   wordle6.css 	new file:   wordle6.html 	new file:   wordle6.js 	new file:   wordle7.css 	new file:   wordle7.html 	new file:   wordle7.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle
 })
