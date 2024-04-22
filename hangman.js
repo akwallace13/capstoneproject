859921f19785505c81440232610cc7a08fabaf29
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2024-04-22 18:14:12 -0500

    modified:   .DS_Store 	new file:   hangman.css 	new file:   hangman.html 	new file:   hangman.js 	new file:   images/empty.png 	new file:   images/hangman-0.png 	new file:   images/hangman-1.png 	new file:   images/hangman-10.png 	new file:   images/hangman-2.png 	new file:   images/hangman-3.png 	new file:   images/hangman-4.png 	new file:   images/hangman-5.png 	new file:   images/hangman-6.png 	new file:   images/hangman-7.png 	new file:   images/hangman-8.png 	new file:   images/hangman-9.png 	new file:   images/lost.gif 	new file:   images/lost2.gif 	new file:   images/victory.gif

---
+++ hangman.js
@@ -0,0 +1,8 @@
+//click effect for sidebar for mobile users/smaller screens
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
